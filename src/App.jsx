7f438685a5bbf1077448a5ac5e9c29758dd2bd77
Dvir <silentwisdom75@gmail.com>
2024-03-06 13:51:43 +0200

    add basic layout UI for email index page

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ import {
 
 import { Home } from './pages/Home';
 import { AboutUs } from './pages/AboutUs';
+import { EmailIndex } from './pages/EmailIndex';
 
 export function App() {
   return (
     <section className="main-app">
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/" element={<Navigate to="/mail" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
+          <Route path="/mail" element={<EmailIndex />} />
         </Routes>
       </Router>
     </section>
---
+++ src/pages/EmailIndex.jsx
@@ -0,0 +1,10 @@
+export function EmailIndex() {
+  return (
+    <section className="email-index">
+      <header className="email-index-header"></header>
+      <nav className="navbar"></nav>
+      <main className="email-index-main"></main>
+      <aside className="addon-list-container"></aside>
+    </section>
+  );
+}
