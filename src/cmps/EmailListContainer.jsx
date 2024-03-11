ba9841bcb3ccecb6319114746a2110674050167b
Dvir <silentwisdom75@gmail.com>
2024-03-11 17:25:20 +0200

    refactor

--- src/cmps/EmailListContainer.jsx
+++ src/cmps/EmailListContainer.jsx
@@ -1,5 +1,13 @@
+import {
+  MdOutlineStarBorder as StarIconOutlineIcon,
+  MdOutlineStar as StarIconIcon,
+  MdDelete as DeleteIcon,
+  MdMarkunread as UnReadIcon,
+  MdOutlineMarkEmailRead as ReadIcon,
+} from 'react-icons/md';
+
 import { EmailFilter } from './EmailFilter';
-import { EmailList } from './EmailList';
+import { EmailPreview } from './EmailPreview';
 
 export function EmailListContainer({
   emails,
@@ -23,12 +31,31 @@ export function EmailListContainer({
       <header className="email-list-header">
         <EmailFilter filterBy={filterBy} onSetFilter={onSetFilter} />
       </header>
-      <EmailList
-        emails={emails}
-        onRemoveEmail={onRemoveEmail}
-        onChangeEmailRead={onChangeEmailRead}
-        onChangeEmailStarred={onChangeEmailStarred}
-      />
+      <ul className="email-list">
+        {emails.map((email) => (
+          <li
+            key={email.id}
+            className={`email-item ${email.isRead ? 'status-read' : ''}`}
+          >
+            <EmailPreview key={email.id} email={email} />
+            <div className="actions-buttons">
+              <button
+                onClick={() => {
+                  onChangeEmailStarred(email);
+                }}
+              >
+                {email.isStarred ? <StarIconIcon /> : <StarIconOutlineIcon />}
+              </button>
+              <button onClick={() => onChangeEmailRead(email)}>
+                {email.isRead ? <ReadIcon /> : <UnReadIcon />}
+              </button>
+              <button onClick={() => onRemoveEmail(email.id)}>
+                <DeleteIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/cmps/EmailPreview.jsx
+++ src/cmps/EmailPreview.jsx
@@ -1,49 +1,17 @@
 import { Link, useParams } from 'react-router-dom';
-import {
-  MdOutlineStarBorder as StarIconOutlineIcon,
-  MdOutlineStar as StarIconIcon,
-  MdDelete as DeleteIcon,
-  MdMarkunread as UnReadIcon,
-  MdOutlineMarkEmailRead as ReadIcon,
-} from 'react-icons/md';
 
-export function EmailPreview({
-  email,
-  onRemoveEmail,
-  onChangeEmailRead,
-  onChangeEmailStarred,
-}) {
+export function EmailPreview({ email }) {
   const params = useParams();
-
-  function onClickEmailActions(ev) {
-    ev.stopPropagation();
-    ev.preventDefault();
-  }
 
   return (
     <Link
       to={`/email/${params.folder}/${email.id}`}
-      className={`email-preview${email.isRead ? ' email-status-read' : ''}`}
+      className={'email-preview'}
     >
       <span className="email-title">{email.from}</span>
       <span className="email-content">
         {email.subject} {email.body}
       </span>
-      <div className="email-actions" onClick={onClickEmailActions}>
-        <button
-          onClick={() => {
-            onChangeEmailStarred(email);
-          }}
-        >
-          {email.isStarred ? <StarIconIcon /> : <StarIconOutlineIcon />}
-        </button>
-        <button onClick={() => onChangeEmailRead(email)}>
-          {email.isRead ? <ReadIcon /> : <UnReadIcon />}
-        </button>
-        <button onClick={() => onRemoveEmail(email.id)}>
-          <DeleteIcon />
-        </button>
-      </div>
     </Link>
   );
 }
