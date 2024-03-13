e3efdba9c79d041c79b4dc4103684d342770cf47
Dvir <silentwisdom75@gmail.com>
2024-03-13 21:30:46 +0200

    Replace buttons with IconButton

--- src/cmps/EmailList.jsx
+++ src/cmps/EmailList.jsx
@@ -3,6 +3,7 @@ import StarIcon from '@mui/icons-material/Star';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MarkEmailUnreadIcon from '@mui/icons-material/MarkEmailUnread';
 import DraftsIcon from '@mui/icons-material/Drafts';
+import { IconButton } from '@mui/material';
 
 import { EmailPreview } from './EmailPreview';
 
@@ -24,21 +25,21 @@ export function EmailList({ emails, onUpdateEmail, onRemoveEmail }) {
           key={email.id}
           className={`email-item ${email.isRead ? 'status-read' : ''}`}
         >
+          <IconButton
+            onClick={() => {
+              onChangeEmailStarred(email);
+            }}
+          >
+            {email.isStarred ? <StarIcon /> : <StarBorderIcon />}
+          </IconButton>
           <EmailPreview key={email.id} email={email} />
           <div className="actions-buttons">
-            <button
-              onClick={() => {
-                onChangeEmailStarred(email);
-              }}
-            >
-              {email.isStarred ? <StarIcon /> : <StarBorderIcon />}
-            </button>
-            <button onClick={() => onChangeEmailRead(email)}>
+            <IconButton onClick={() => onChangeEmailRead(email)}>
               {email.isRead ? <MarkEmailUnreadIcon /> : <DraftsIcon />}
-            </button>
-            <button onClick={() => onRemoveEmail(email.id)}>
+            </IconButton>
+            <IconButton onClick={() => onRemoveEmail(email.id)}>
               <DeleteIcon />
-            </button>
+            </IconButton>
           </div>
         </li>
       ))}
